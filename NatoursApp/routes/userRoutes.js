7865647bbaeed27cf0282ed69bbc89e6b62bcbe7
amirAghayari <amiraghayari2119@gmail.com>
2025-09-15 20:05:42 +0330

    update users & tours

--- NatoursApp/routes/userRoutes.js
+++ NatoursApp/routes/userRoutes.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const userController = require("../controllers/userController");
 
 const router = express.Router();
+
+router.route("/").get(userController.getAllUsers).post(userController.createUser);
+
+router.route("/:id").get(userController.getUsers).patch(userController.updateUser).delete(userController.deleteUser);
 
 module.exports = router;
